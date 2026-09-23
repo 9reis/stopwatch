d07050457267928bcc73b635f351e4c95086967d
9reis <lucasreis_9@hotmail.com>
2026-09-23 16:21:37 -0300

    Implementa useState e useEffect

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const [time, setTime] = useState(0)
+  const [running, setRunning] = useState(false);
 
-    </div>
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
+  return (
+    <>
+      <h1>Stopatch</h1>
+      <div>
+        <span>{("0" + Math.floor((time / 6000) % 60)).slice(-2)} : </span>
+        <span>{("0" + Math.floor((time / 100) % 60)).slice(-2)} : </span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        <button onClick={() => setRunning(true)} >Start</button>
+        <button onClick={() => setRunning(false)} >Stop</button>
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
+    </>
   );
 }
 
